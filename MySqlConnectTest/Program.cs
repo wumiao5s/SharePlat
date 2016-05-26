b251ecd54b7645b3e76fe4b36ac4258d6fe7cc88
wumiao5s <15950060547@163.com>
2016-05-26 14:20:58 +0000

    Add files via upload
    MySqlConnectTest

---
+++ MySqlConnectTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using SharePlat.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySqlConnectTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sql = "select * from testdb";
+            DataTable dt = MySqlHelper.ExecuteDataTable(sql);
+            foreach (DataRow row in dt.Rows)
+            {
+                Console.WriteLine(row["id"] + ":" + row["name"]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
